4ea771a1a234c6244b0cd44049b20d05d936db08
yuwei217 <yjia0142@student.monash.edu>
2021-10-22 00:41:49 +1100

    add readme

---
+++ R/key_crop_yields.R
@@ -0,0 +1,21 @@
+
+
+#' Title
+#'
+#' @param x
+#'
+#' @section Background:
+#' The data was colected from ...
+#'
+#'
+#' @format A data frame with 53940 rows and 10 variables:
+#' \describe{
+#'   \item{price}{price, in US dollars}
+#'   \item{carat}{weight of the diamond, in carats}
+#'   ...
+#' }
+#'
+#' @source Where you got the data
+#' @examples
+#' print(key_crop_yields)
+"key_crop_yields"
